bb26edcff2b03f5bb65afa970de56fed20036884
zacharymiller <zacharymiller0818@gmail.com>
2023-09-05 11:57:28 -0400

    complete the task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import BouncingAndRotatingPuzzlePiece from "./components/BouncingAndRotatingPuzzlePiece";
+import PuzzlePiece from "./components/PuzzlePiece";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BouncingAndRotatingPuzzlePiece />
+      <PuzzlePiece
+        primaryColor="#004973" // color of stator
+        secondaryColor="#03ae9f" // color of rotator
+        ballColor="#e8e8e8" // color of ball
+        thickness={8} // thhikcness
+        bouncingTime={0.6} // bouncingTime
+        length={240}
+        slideOutDistance1={40} // top-left piece
+        slideOutDistance2={40} // bottom-right piece
+        rotationAngle={12}     // Rotation angle deg: counter clockwise
+      />
     </div>
   );
 }
--- src/components/PuzzlePiece.js
+++ src/components/PuzzlePiece.js
@@ -1,21 +1,27 @@
-import React, { useLayoutEffect } from "react";
+import React, { useEffect } from "react";
 import { gsap } from "gsap";
 
 const PuzzlePiece = ({
-  primary = "black",
-  secondary = "green",
-  ballcolor = "grey",
+  primaryColor = "#004973",
+  secondaryColor = "#03ae9f",
+  ballColor = "#e8e8e8",
+  thickness = 8,
+  bouncingTime = 0.6,
+  length = "240",
+  slideOutDistance1 = 40,
+  slideOutDistance2 = 40,
+  rotationAngle = 12,
 }) => {
-  useLayoutEffect(() => {
+  useEffect(() => {
     const tl = gsap.timeline({ repeat: -1 });
 
     tl.fromTo(
-      "#stator",
+      ".stator",
       { transformOrigin: "50% 50%", rotation: 5 },
       {
-        rotation: -12,
+        rotation: -rotationAngle,
         ease: "power1.inOut",
-        duration: 0.6,
+        duration: bouncingTime,
         repeat: 3,
         yoyo: true,
       }
@@ -23,11 +29,11 @@ const PuzzlePiece = ({
       .to(
         "#rotator1",
         {
-          x: -40,
-          y: -40,
+          x: -slideOutDistance1,
+          y: -slideOutDistance1,
           rotate: 0,
           ease: "power1.inOut",
-          duration: 0.6,
+          duration: bouncingTime,
           repeat: 1,
           yoyo: true,
         },
@@ -35,47 +41,47 @@ const PuzzlePiece = ({
       )
       .fromTo(
         "#rotator1",
-        { transformOrigin: "100% 100%", rotation: 5 },
+        { transformOrigin: "50% 50%", rotation: 5 },
         {
-          rotation: -12,
+          rotation: -rotationAngle,
           ease: "power1.inOut",
-          duration: 0.6,
+          duration: bouncingTime,
           repeat: 1,
           yoyo: true,
         },
-        1.2
+        bouncingTime * 2
       )
       .to(
         "#rotator2",
         {
-          x: 40,
-          y: 40,
+          x: slideOutDistance2,
+          y: slideOutDistance2,
           rotate: 0,
           ease: "power1.inOut",
-          duration: 0.6,
+          duration: bouncingTime,
           repeat: 1,
           yoyo: true,
         },
-        1.2
+        bouncingTime * 2
       )
       .fromTo(
         "#rotator2",
-        { transformOrigin: "0% 0%", rotation: 5 },
+        { transformOrigin: "50% 50%", rotation: 5 },
         {
-          rotation: -12,
+          rotation: -rotationAngle,
           ease: "power1.inOut",
-          duration: 0.6,
+          duration: bouncingTime,
           repeat: 1,
           yoyo: true,
         },
         0
       )
-      tl.to(
+      .to(
         "#ball",
         {
           rotation: "-=180",
-          duration: 1.2,
-          transformOrigin: "7x -43px",
+          duration: bouncingTime * 2,
+          transformOrigin: "7x 57px",
           ease: "back.in(1.7)",
         },
         0
@@ -84,78 +90,98 @@ const PuzzlePiece = ({
         "#ball",
         {
           rotation: "-=180",
-          duration: 1.2,
-          transformOrigin: "7x -43px",
+          duration: bouncingTime * 2,
+          transformOrigin: "7x 57px",
           ease: "back.in(1.7)",
         },
-        1.2
-      )
-  }, []);
+        bouncingTime * 2
+      );
+
+    return () => tl.kill();
+  }, [bouncingTime, slideOutDistance1, slideOutDistance2, rotationAngle]);
 
   return (
     <>
       <svg
-        id="entire"
-        width="200"
-        height="200"
-        viewBox="-50 -50 300 300"
+        width={length}
+        height={length}
+        viewBox="0 0 240 240"
+        strokeLinejoin="round"
+        strokeWidth={thickness}
         xmlns="http://www.w3.org/2000/svg"
+        style={{ overflow: "visible" }}
       >
+        {/* Puzzle Board Piece1 */}
         <path
-          id="stator"
-          d="M100 3
-            A97 97 0 0 1 197 100
-            L3 100
-            A97 97 0 0 0 100 197
-            L100 165
-            A15 15 0 0 0 100 135
-            L100 65
-            A15 15 0 0 1 100 35
-            Z"
+          className="stator"
+          d="M120 20
+            A100 100 0 0 1 220 120
+            L20 120
+            A100 100 0 0 0 120 220
+            "
           fill="none"
-          stroke={primary}
-          strokeWidth="6"
-          strokeLinejoin="round"
+          stroke={primaryColor}
         />
-        <path
-          id="rotator1"
-          d="M100 3
-              A97 97 0 0 0 3 100
-              L35 100
-              A15 15 0 0 0 65 100
-              L100 100
+
+        {/* Left Top Bouncing Piece */}
+        <g id="rotator1">
+          <path
+            d="M120 20
+              A100 100 0 0 0 20 120
+              L55 120
+              A15 15 0 0 0 85 120
+              L120 120
               Z"
-          fill="none"
-          stroke={secondary}
-          strokeWidth="6"
-          strokeLinejoin="round"
-        />
-        <path
-          id="rotator2"
-          d="M100 100
-              L100 197
-              A97 97 0 0 0 197 100
-              L165 100
-              A15 15 0 0 0 135 100
-              L100 100
+            fill="white"
+            stroke={secondaryColor}
+          />
+          <line x1="120" y1="120" x2="220" y2="220" stroke="none" />
+        </g>
+
+        {/* Right Bottom Bouncing Piece */}
+        <g id="rotator2">
+          <path
+            d="M120 120
+              L120 220
+              A100 100 0 0 0 220 120
+              L185 120
+              A15 15 0 0 0 155 120
+              L120 120
               Z"
-          fill="none"
-          stroke={secondary}
-          strokeWidth="6"
-          strokeLinejoin="round"
+            fill="white"
+            stroke={secondaryColor}
+          />
+          <line x1="120" y1="120" x2="20" y2="20" stroke="none" />
+        </g>
+
+        {/* Puzzle Board Piece2 */}
+        <path
+          className="stator"
+          fill="white"
+          stroke={primaryColor}
+          d="
+          M206.6 70
+          A100 100 0 0 0 120 20
+          L120 55
+          A15 15 0 0 0 120 85
+          L120 155
+          A15 15 0 0 1 120 185
+          L120 220
+          A100 100 0 0 1 33.4 170"
         />
+
+        {/* Bouncing Ball Piece */}
         <circle
           id="ball"
-          cx="100"
-          cy="150"
+          cx="120"
+          cy="70"
           r="7"
           fill="none"
-          stroke={ballcolor}
+          stroke={ballColor}
           strokeWidth="7"
         />
       </svg>
     </>
   );
 };
-
 export default PuzzlePiece;
